0b0a9ccd1cdf5497eb15f14c182d37e08921f65e
Documenter.jl <documenter@juliadocs.github.io>
2021-12-14 12:26:17 +0000

    build based on 1d1dfa0

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RandomLineChart","category":"page"},{"location":"#RandomLineChart","page":"Home","title":"RandomLineChart","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RandomLineChart.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RandomLineChart]","category":"page"}]
+}
